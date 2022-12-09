b319a0876446e2b44ce61e6d3d39205ac085ef03
a.aldarov <alex_aldarov@mail.ru>
2022-12-09 15:58:11 +0800

    fix

--- dags/tasks.py
+++ dags/tasks.py
@@ -4,6 +4,10 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+from airflow.sensors.python import PythonSensor
+from airflow.operators.python import BranchPythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.exceptions import AirflowFailException
 
 
 path_to_file = os.path.join(os.path.dirname(__file__), 'res.txt')
@@ -33,6 +37,32 @@ def delete_last_line_file():
             if len(last_line) < 2:
                 file.truncate(pos)
 
+def get_result_from_file():
+    with open(path_to_file, "rb+") as file:
+        try:
+            file.seek(-2, os.SEEK_END)
+            while file.read(1) != b'\n':
+                file.seek(-2, os.SEEK_CUR)
+        except OSError:
+            file.seek(0)
+        last_line = file.readline().decode().split(' ')
+        if len(last_line) < 2:
+            return int(last_line[0])
+        return 0
+
+def calc_result():
+    result = 0
+    with open(path_to_file) as file:
+        lines = file.readlines()[:-1]
+        sum_col_1 = 0
+        sum_col_2 = 0
+        for line in lines:
+            digits = line.split(' ')
+            sum_col_1 += int(digits[0])
+            sum_col_2 += int(digits[1])
+        result = sum_col_1 - sum_col_2
+    return result
+
 def result_calculation():
     delete_last_line_file()
     result = 0
@@ -48,10 +78,36 @@ def result_calculation():
     with open(path_to_file, "a", encoding = 'utf-8') as file:
         file.write(str(result)+"\n")
 
-with DAG(dag_id="first_dag", start_date=datetime(2022, 1, 1), schedule="20-25 16 * * *") as dag:
-    hello_task = BashOperator(task_id="hello", bash_command="echo hello")
-    python_task1 = PythonOperator(task_id="task1", python_callable = hello)
-    add_numbers_to_file = PythonOperator(task_id="add_numbers_to_file", python_callable = add_numbers_to_file)
-    result_calculation = PythonOperator(task_id="result_calculation", python_callable = result_calculation)
+def check_file():
+    if os.path.isfile(path_to_file) and calc_result() == get_result_from_file():
+        return True
 
-    hello_task >> python_task1 >> add_numbers_to_file >> result_calculation
+    return False
+
+def branch_check_file():
+    if os.path.isfile(path_to_file) and os.path.getsize(path_to_file) > 0:
+        return "create_db"
+    return "task_to_fail"
+
+def task_to_fail():
+    raise AirflowFailException("Входной файл пуст или его не существует")
+
+def get_connection():
+    return ""
+
+def create_db():
+    return False
+
+# with DAG(dag_id="first_dag", start_date=datetime(2022, 1, 1), schedule="20-25 16 * * *") as dag:
+#     create_db = DummyOperator(task_id='create_db')
+
+#     hello_task = BashOperator(task_id="hello", bash_command="echo hello")
+#     python_task1 = PythonOperator(task_id="task1", python_callable = hello)
+#     add_numbers_to_file = PythonOperator(task_id="add_numbers_to_file", python_callable = add_numbers_to_file)
+#     result_calculation = PythonOperator(task_id="result_calculation", python_callable = result_calculation)
+#     check_file = PythonSensor(task_id="check_file", python_callable=check_file)
+#     branch_check_file = BranchPythonOperator(task_id='branch_check_file', python_callable=branch_check_file)
+#     task_to_fail = PythonOperator(task_id="task_to_fail", python_callable = task_to_fail)
+#     create_db = PythonOperator(task_id="create_db", python_callable = create_db)
+
+#     hello_task >> python_task1 >> add_numbers_to_file >> result_calculation >> check_file >> branch_check_file >> [create_db, task_to_fail]
