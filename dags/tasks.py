a8acfd154fd4844a9d5c7948a1dd85f163bfa05c
Aldarov <alex_aldarov@mail.ru>
2022-12-11 20:46:28 +0800

    fix

--- dags/tasks.py
+++ dags/tasks.py
@@ -1,5 +1,6 @@
 import random
 import os
+import psycopg2
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -8,6 +9,7 @@ from airflow.sensors.python import PythonSensor
 from airflow.operators.python import BranchPythonOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.exceptions import AirflowFailException
+from airflow.hooks.base_hook import BaseHook
 
 
 path_to_file = os.path.join(os.path.dirname(__file__), 'res.txt')
@@ -92,22 +94,65 @@ def branch_check_file():
 def task_to_fail():
     raise AirflowFailException("Входной файл пуст или его не существует")
 
-def get_connection():
-    return ""
+# pg_host = "host.docker.internal"
+# pg_host = "localhost"
+# pg_port = "5432"
+# pg_user = "postgres"
+# pg_password = "postgres"
+# pg_database = "Test"
 
 def create_db():
-    return False
+    # conn = psycopg2.connect(host=pg_host, port=pg_port, user=pg_user, password=pg_password, database=pg_database)
+    connection = BaseHook.get_connection("postgres_conn")
+    conn = psycopg2.connect(host=connection.host, port=connection.port, user=connection.login, password=connection.password, database=connection.schema)
+    cursor = conn.cursor()
+    cursor.execute("create table if not exists data(value_1 integer NOT NULL, value_2 integer NOT NULL)")
+    with open(path_to_file) as file:
+        lines = file.readlines()[:-1]
+        for line in lines:
+            digits = line.split(' ')
+            value_1 = int(digits[0])
+            value_2 = int(digits[1])
+            cursor.execute(f"insert into data(value_1, value_2) values({value_1}, {value_2})")
+    conn.commit()
 
-# with DAG(dag_id="first_dag", start_date=datetime(2022, 1, 1), schedule="20-25 16 * * *") as dag:
-#     create_db = DummyOperator(task_id='create_db')
+    cursor.close()
+    conn.close()
 
-#     hello_task = BashOperator(task_id="hello", bash_command="echo hello")
-#     python_task1 = PythonOperator(task_id="task1", python_callable = hello)
-#     add_numbers_to_file = PythonOperator(task_id="add_numbers_to_file", python_callable = add_numbers_to_file)
-#     result_calculation = PythonOperator(task_id="result_calculation", python_callable = result_calculation)
-#     check_file = PythonSensor(task_id="check_file", python_callable=check_file)
-#     branch_check_file = BranchPythonOperator(task_id='branch_check_file', python_callable=branch_check_file)
-#     task_to_fail = PythonOperator(task_id="task_to_fail", python_callable = task_to_fail)
-#     create_db = PythonOperator(task_id="create_db", python_callable = create_db)
+def add_result_to_db():
+    # conn = psycopg2.connect(host=pg_host, port=pg_port, user=pg_user, password=pg_password, database=pg_database)
+    connection = BaseHook.get_connection("postgres_conn")
+    conn = psycopg2.connect(host=connection.host, port=connection.port, user=connection.login, password=connection.password, database=connection.schema)
+    cursor = conn.cursor()
+    cursor.execute("delete from data")
+    cursor.execute("alter table data ADD COLUMN coef integer;")
+    sum_col_1 = 0
+    sum_col_2 = 0
+    with open(path_to_file) as file:
+        lines = file.readlines()[:-1]
+        for line in lines:
+            digits = line.split(' ')
+            value_1 = int(digits[0])
+            value_2 = int(digits[1])
+            sum_col_1 += value_1
+            sum_col_2 += value_2
+            coef = sum_col_1 - sum_col_2
+            cursor.execute(f"insert into data(value_1, value_2, coef) values({value_1}, {value_2}, {coef})")
+    conn.commit()
 
-#     hello_task >> python_task1 >> add_numbers_to_file >> result_calculation >> check_file >> branch_check_file >> [create_db, task_to_fail]
+    cursor.close()
+    conn.close()
+
+
+with DAG(dag_id="first_dag", start_date=datetime(2022, 12, 11), schedule="20-25 16 * * *") as dag:
+    hello_task = BashOperator(task_id="hello", bash_command="echo hello")
+    python_task1 = PythonOperator(task_id="task1", python_callable = hello)
+    add_numbers_to_file = PythonOperator(task_id="add_numbers_to_file", python_callable = add_numbers_to_file)
+    result_calculation = PythonOperator(task_id="result_calculation", python_callable = result_calculation)
+    check_file = PythonSensor(task_id="check_file", python_callable=check_file)
+    branch_check_file = BranchPythonOperator(task_id='branch_check_file', python_callable=branch_check_file)
+    task_to_fail = PythonOperator(task_id="task_to_fail", python_callable = task_to_fail)
+    create_db = PythonOperator(task_id="create_db", python_callable = create_db)
+    add_result_to_db = PythonOperator(task_id="add_result_to_db", python_callable = add_result_to_db)
+
+    hello_task >> python_task1 >> add_numbers_to_file >> result_calculation >> check_file >> branch_check_file >> [create_db, task_to_fail] >> add_result_to_db
